a537f43befd0f131e29a55ffd33cbc387779f45d
weirdRafi <mazharulrafi@gmail.com>
2023-02-17 23:16:00 +0600

    third commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 serial = 0;
+// For Triangle 
 document.getElementById('btn-triangle').addEventListener('click', function(){
     serial = serial + 1;
     const triangleInputOne = document.getElementById('triangle-1').value;
@@ -10,9 +11,6 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     const triangleInput = 0.5 * triangleInputOneString * triangleInputTwoString;
     const triangle = document.getElementById('first-name').innerText;
 
-
-
-
     const container = document.getElementById("table");
     const tr = document.createElement("tr");
     tr.innerHTML = `
@@ -20,6 +18,81 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
       <td>${triangle}</td>
       <td>${triangleInput}</td>
       
+      
+    `;
+    container.appendChild(tr);
+
+})
+
+// For Rectangle
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    serial = serial + 1;
+    const rectangleInputOne = document.getElementById('rectangle-1').value;
+    const rectangleInputOneString = parseFloat(rectangleInputOne);
+
+    const rectangleInputTwo = document.getElementById('rectangle-2').value;
+    const rectangleInputTwoString = parseFloat(rectangleInputTwo);
+
+    const rectangleInput = rectangleInputOneString * rectangleInputTwoString;
+    const rectangle = document.getElementById('second-name').innerText;
+
+    const container = document.getElementById("table");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${rectangle}</td>
+      <td>${rectangleInput}</td>
+      
+      
+      
+    `;
+    container.appendChild(tr);
+})
+
+// For Parallelogram
+
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    serial = serial + 1;
+    const parallelogramInputOne = document.getElementById('parallelogram-1').innerText;
+    const parallelogramInputOneString = parseFloat(parallelogramInputOne);
+
+    const parallelogramInputTwo = document.getElementById('parallelogram-2').innerText;
+    const parallelogramInputTwoString = parseFloat(parallelogramInputTwo);
+
+    const parallelogramInput = parallelogramInputOneString * parallelogramInputTwoString;
+    const parallelogram = document.getElementById('third-name').innerText;
+
+    const container = document.getElementById("table");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${parallelogram}</td>
+      <td>${parallelogramInput}</td>
+      
+    `;
+    container.appendChild(tr);
+
+})
+
+// For Rhombus
+
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    serial = serial + 1;
+    const rhombusInputOne = document.getElementById('rhombus-1').innerText;
+    const rhombusInputOneString = parseFloat(rhombusInputOne);
+
+    const rhombusInputTwo = document.getElementById('rhombus-1').innerText;
+    const rhombusInputTwoeString = parseFloat(rhombusInputTwo);
+
+    const rhombusInput = rhombusInputOneString * rhombusInputTwoeString;
+    const rhombus = document.getElementById('fourth-name').innerText;
+
+    const container = document.getElementById("table");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${rhombus}</td>
+      <td>${rhombusInput}</td>
       
     `;
     container.appendChild(tr);
